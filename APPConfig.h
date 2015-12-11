eafb6ade39e703dad4ddfbc33cb269f6d08c496f
magic3584 <xuewuzhiyuan@sina.com>
2015-12-11 09:40:30 +0800

    add macro

--- APPConfig.h
+++ APPConfig.h
@@ -5,7 +5,9 @@
 #define SCREEN_WIDTH                [[UIScreen mainScreen] bounds].size.width
 #define SCREEN_HEIGHT               [[UIScreen mainScreen] bounds].size.height
 #define IS_3_5INCH   ([[UIScreen mainScreen]bounds].size.height == 480)
-
+#define IS_4_0INCH   ([[UIScreen mainScreen]bounds].size.height == 568)
+#define IS_4_7INCH   ([[UIScreen mainScreen]bounds].size.height == 667)
+#define IS_5_5INCH   ([[UIScreen mainScreen]bounds].size.height == 736)
 
 
 #define PATH_OF_DOCUMENT            [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]
